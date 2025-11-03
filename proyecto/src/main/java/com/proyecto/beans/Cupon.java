d3a943b8c4c13542ea1f497e632cb8c322842cf7
diegorodriguez70 <diegorf70@educastur.es>
2025-11-03 20:09:53 +0100

    Añadida la relacion many2 many entre restaurante y cupon

--- proyecto/src/main/java/com/proyecto/beans/Cupon.java
+++ proyecto/src/main/java/com/proyecto/beans/Cupon.java
@@ -1,11 +1,15 @@
 package com.proyecto.beans;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,5 +25,7 @@ public class Cupon {
 	private int idCupon;
 	private String tiempoDuracion;
 	private BigDecimal descuento;
-
+	
+	@OneToMany(mappedBy = "cupon", cascade = CascadeType.REMOVE, orphanRemoval = true)
+	private List<Pertenece> pertenece = new ArrayList<>();
 }
