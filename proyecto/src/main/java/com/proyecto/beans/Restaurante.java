b13fcd12470fa21acccf9583faa3754a350ff4ca
diegorodriguez70 <diegorf70@educastur.es>
2025-11-19 13:32:58 +0100

    Vistas de restaurantes completa y funcional final

--- proyecto/src/main/java/com/proyecto/beans/Restaurante.java
+++ proyecto/src/main/java/com/proyecto/beans/Restaurante.java
@@ -45,7 +45,7 @@ public class Restaurante {
 	@OneToMany(mappedBy = "restaurante", cascade = CascadeType.REMOVE, orphanRemoval = true)
 	private List<Pertenece> pertenece = new ArrayList<>();
 	
-	@OneToMany(mappedBy = "restaurante")
+	@OneToMany(mappedBy = "restaurante", cascade = CascadeType.REMOVE, orphanRemoval = true)
 	private List<Reserva> reservas = new ArrayList<>();
 
 
